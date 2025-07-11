038b034a7974e553cbd11c20571c6389215a694d
Eduardo San Martin Morote <posva13@gmail.com>
2025-07-11 15:07:06 +0200

    build for deploy

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+rm -rf dist
+
+pnpm exec slidev build slides.md --base / --out dist
+
+pnpm exec slidev build slides.zh.md --base /zh/ --out dist/zh
+
+rm dist/_redirects
+rm dist/zh/_redirects
